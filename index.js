194a775ff2667c3175f579bcb3a509196e8c0683
Swapnil Akulwar <swapnilakulwar97@gmail.com>
2022-08-05 23:11:06 -0700

    it is java-script file

---
+++ index.js
@@ -0,0 +1,12 @@
+document.write("Hello World");
+const arr=[1,2,5,4,6];
+let sum=0;
+for(i=0;i<arr.length;i++){
+sum+=arr[i];
+}
+console.log(sum);
+
+
+const array=[1,2,5,4,6,64];
+let max = Math.min(...arr);
+console.log(max);
